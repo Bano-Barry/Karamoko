6630c4fa2ae9c30c15941b6556e18f0dbc41e33f
Bano-Barry <barrymamadoubano14@gmail.com>
2025-04-13 16:36:16 +0000

    problemOk: models core

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,5 @@
 from django.contrib import admin
-from .models import (
-    TypeFormation, Competence, Niveau, Repetiteur, Cours,
-    Souscripteur, PlanTarifaire, Souscription, MethodePaiement, Paiement
-)
+from .models import  Niveau
 
-admin.site.register(TypeFormation)
-admin.site.register(Competence)
+
 admin.site.register(Niveau)
-admin.site.register(Repetiteur)
-admin.site.register(Cours)
-admin.site.register(Souscripteur)
-admin.site.register(PlanTarifaire)
-admin.site.register(Souscription)
-admin.site.register(MethodePaiement)
-admin.site.register(Paiement)
