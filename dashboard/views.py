ead79339f9539530e9739f7770b9f383eb841ef5
Bano-Barry <barrymamadoubano14@gmail.com>
2025-05-23 14:44:00 +0000

    personnalisation du dashboard en fonction de l'utlisateur connecte

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,18 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard_home(request):
-    return render(request, 'dashboard/home.html')  # Page d'accueil avec la structure globale
+    user = request.user
+    context = {}
+
+    if user.role == 'repetiteur':
+        context['dashboard_type'] = 'repetiteur'
+        # tu peux précharger des stats, cours, etc.
+    elif user.role == 'parent':
+        context['dashboard_type'] = 'parent'
+        # tu peux charger les souscriptions, enfants, etc.
+    elif user.is_superuser:
+        context['dashboard_type'] = 'admin'
+        # même logique ici
+    return render(request, 'dashboard/home.html', context)  # Page d'accueil avec la structure globale
 
 # Create your views here.
